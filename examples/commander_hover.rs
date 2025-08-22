2198b4fa21d1827329d99bbb879e6d3de94d8f46
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2025-08-22 17:16:53 +0200

    Rename hover and position examples to specify they are for (regular) commander

--- examples/commander_hover.rs
+++ examples/commander_hover.rs
@@ -2,14 +2,14 @@ use crazyflie_link::LinkContext;
 use crazyflie_lib::Crazyflie;
 use tokio::time::{sleep, Duration};
 
-/// Example that demonstrates hover setpoint control
+/// Commander example that demonstrates hover setpoint control
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let context = LinkContext::new();
     let crazyflie = Crazyflie::connect_from_uri(
         &context,
-        "radio://0/80/2M/E7E7E7E7E7",
+        "radio://0/90/2M/F00D2BEFED",
     )
     .await?;
 
@@ -59,6 +59,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Stop the motors
     crazyflie.commander.setpoint_stop().await?;
+
+    // Notify the Crazyflie that the low-level setpoint has stopped
+    crazyflie.commander.notify_setpoint_stop(0).await?;
+
     println!("Hover setpoint example complete. Motors stopped.");
     Ok(())
 }
