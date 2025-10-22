c7e2ca310d64f6e9727bb7c6d292639e3bb04d8b
Marcus <marcus.c.eliasson@gmail.com>
2025-10-22 10:51:18 +0200

    Check bound when reading data from memory

--- src/subsystems/memory/memory_types.rs
+++ src/subsystems/memory/memory_types.rs
@@ -60,10 +60,15 @@ impl MemoryBackend {
                 .await;
             if let Ok(pk) = pk {
                 let pk_data = pk.get_data();
+
+                if pk_data.len() < 6 {
+                    return Err(Error::MemoryError("Malformed memory read response".into()));
+                }
+
                 let read_address = u32::from_le_bytes(pk_data[1..5].try_into().unwrap());
                 let status = pk_data[5];
 
-                if pk_data.len() >= 5 && read_address == current_address as u32 && status == 0 {
+                if pk_data.len() >= 6 && read_address == current_address as u32 && status == 0 {
                     let read_data = &pk_data[6..];
                     let start = (current_address - address) as usize;
                     let end = start + read_data.len();
